72bbc5fc7a228420e95c3cffe378ea50a7c31e42
simplg <81249731+simplg@users.noreply.github.com>
2022-01-10 17:01:48 +0100

    Add api example

--- Api/Api/Api.py
+++ Api/Api/Api.py
@@ -1,1 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
+
+app = FastAPI()
+
+
+class Article(BaseModel):
+    title: str
+    contenu: str
+    tags: List[str] = []
+
+
+articles: List[Article] = [
+    Article(title="Test 3", contenu="Mon contenu de test", tags=["lol", "test", "mdr"]),
+    Article(title="Test 2", contenu="Mon contenu de test", tags=["lol", "test", "mdr"]),
+    Article(title="Test 1", contenu="Mon contenu de test", tags=["lol", "test", "mdr"]),
+]
+
+
+@app.get("/", response_model=List[Article])
+def index():
+    return articles
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
